1b9adcb9771f76948d9a38bee803ef51b2d62dc1
joe fleischauer <jfleischauer@rlas-116.org>
2018-08-15 15:03:56 -0500

    Initial Commit

---
+++ FooBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FooBar
+//
+//  Created by Fleischauer, Joseph on 8/15/18.
+//  Copyright © 2018 Fleischauer, Joseph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
